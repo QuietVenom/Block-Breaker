69f8351e3e418051738b3af2a4ee250397f05157
Izcoatl Avila <izco.marcos@gmail.com>
2019-03-09 14:22:27 -0600

    Add Complete Project

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    //configuration parameters
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float maxX = 15f;
+    [SerializeField] float minX = 1f;
+
+    //cache component references
+    GameSession theGameSession;
+    Ball theBall;
+
+    // Use this for initialization
+    void Start () {
+        theGameSession = FindObjectOfType<GameSession>();
+        theBall = FindObjectOfType<Ball>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Input.mousePosition.x / Screen.width * screenWidthInUnits);
+        //float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+	}
+
+    private float GetXPos()
+    {
+        if (theGameSession.IsAutoPlayEnabled())
+        {
+            return theBall.transform.position.x;
+        }
+        else
+        {
+            return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+    
+    public void LoadNextScene()
+    {
+        int currenteSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currenteSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+        FindObjectOfType<GameSession>().ResetGame();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
